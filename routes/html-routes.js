df61407ded7fb7b22ea634c456a608d31b38af2d
Clark Gesen <cgesen@gmail.com>
2020-01-07 21:25:04 -0500

    Added api routes, html routes, server.js, and connection

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,14 +1,18 @@
 const path = require('path');
 const router = require('express').Router();
-
+// dashboard route
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
-
-// Place this route below all others to send he index.html file
+// Place this route below all others to send the index.html file
 // to any request that is not explicitly defined above
 router.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
-
+router.get('/activities', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/activities.html'));
+});
+router.get('/nutrition', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/nutrition.html'));
+});
 module.exports = router;
