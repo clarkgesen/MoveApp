b20bf05ad050909de7a51729e7f83ae08af1bae3
William White <william.white86@gmail.com>
2020-01-28 19:27:15 -0500

    added compression

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const db = require('./models');
 const routes = require('./routes');
+const compression = require('compression')
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -8,6 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 app.use('/', routes);
+app.use(compression())
 // Sync sequelize models then start Express app
 // =============================================
 db.sequelize.sync().then(() => {
